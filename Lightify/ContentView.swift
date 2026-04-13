2e013021631b0e305e2945741ca177175202ec7f
Benji <ben.jonasson@outlook.com>
2026-04-13 14:27:05 +0200

    Initial Commit

---
+++ Lightify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lightify
+//
+//  Created by Benji on 2026-04-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lightify/LightifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightifyApp.swift
+//  Lightify
+//
+//  Created by Benji on 2026-04-13.
+//
+
+import SwiftUI
+
+@main
+struct LightifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
